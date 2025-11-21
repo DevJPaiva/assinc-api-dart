637f1ddce68f872271888f282e72d29d4e18929d
DevJPaiva <jorgefe0210@gmail.com>
2025-11-20 23:00:10 -0300

    Add http package and stub requestData function
    Added the http package as a dependency in pubspec.yaml and pubspec.lock. Introduced a stub requestData() function in main.dart with a URL placeholder for future HTTP requests.

--- assinc-api-dart/bin/main.dart
+++ assinc-api-dart/bin/main.dart
@@ -3,3 +3,7 @@ import 'package:http/http.dart';
 void main() {
   print('Hello world');
 }
+
+requestData(){
+  String url = 'https://gist.githubusercontent.com/DevJPaiva/38432c664e3de820a4f5e22998c6164b/raw/9b96752239bc0f3459a6f0f03b24b2ada6e168e5/accounts.json';
+}
